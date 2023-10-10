2d13eea9d623b2f4dd9cf76bf944775b8de55bd8
Hendri Alqori <hendri.alqori@shintavr.com>
2023-10-10 21:28:46 +0700

    fix : test

---
+++ dist/assets/_index-430e21c9.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-503d53b9.js";function o(){return r.jsx(r.Fragment,{})}export{o as default};
--- dist/assets/_index-748d4ddd.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-8756e970.js";function o(){return r.jsx(r.Fragment,{})}export{o as default};
